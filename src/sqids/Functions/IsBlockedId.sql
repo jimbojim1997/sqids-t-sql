afa085b7aa12bb0fb9913f4729c9be85c23b5d10
James Duda <jimbojim1997@gmail.com>
2023-08-19 22:50:48 +0100

    Add function IsBlockedId.

---
+++ src/sqids/Functions/IsBlockedId.sql
@@ -0,0 +1,8 @@
+﻿CREATE   FUNCTION [sqids].[IsBlockedId] (
+	@Id NVARCHAR(MAX)
+)
+RETURNS BIT
+BEGIN
+	IF EXISTS(SELECT 1 FROM [sqids].[BlockList] WHERE @Id LIKE '%' + Word + '%') RETURN 1
+	RETURN 0
+END
