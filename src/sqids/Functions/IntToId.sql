b60ecb90b341916a71570d9b200281ec6fad7b2a
James Duda <jimbojim1997@gmail.com>
2023-08-19 23:10:56 +0100

    Add function IntToId.

---
+++ src/sqids/Functions/IntToId.sql
@@ -0,0 +1,26 @@
+﻿CREATE   FUNCTION [sqids].[IntToId] (
+	@Value INT,
+	@Alphabet IntCharTable READONLY
+)
+RETURNS NVARCHAR(MAX)
+BEGIN
+	DECLARE @id NVARCHAR(MAX) = '',
+			@length INT = (SELECT COUNT(*) FROM @Alphabet),
+			@c NCHAR(1),
+			@r INT = @Value
+
+	-- TSQL doesn't have do while so manually duiplicate first iteration
+
+	SELECT @c = C FROM @Alphabet WHERE I = @r % @length
+	SET @id = @c + @id
+	SET @r = FLOOR(@r / @length)
+
+	WHILE (@r > 0)
+	BEGIN
+		SELECT @c = C FROM @Alphabet WHERE I = @r % @length
+		SET @id = @c + @id
+		SET @r = FLOOR(@r / @length)
+	END
+
+	RETURN @id
+END
